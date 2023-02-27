c9b32eb95ce78578c55c9e7473ee04cb19700e87
Toriqul Islam Leon <yeitoriqul@gmail.com>
2023-02-27 16:37:30 +0600

    Transform

--- 4-webpack/src/script.js
+++ 4-webpack/src/script.js
@@ -1,3 +1,27 @@
 import "./style.css";
+import * as THREE from "three";
 
-console.log(786);
+// scene
+const scene = new THREE.Scene();
+
+// red cude
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75);
+camera.position.z = 3;
+
+scene.add(camera);
+
+// renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.render(scene, camera);
