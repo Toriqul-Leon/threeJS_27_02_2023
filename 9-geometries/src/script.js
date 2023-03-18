2be4b66b89076b75740034f933462ce709ec73ae
Toriqul Islam Leon <yeitoriqul@gmail.com>
2023-03-19 04:39:31 +0600

    Debug-UI

---
+++ 9-geometries/src/script.js
@@ -0,0 +1,119 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+// axesHelper
+// const axesHelper = new THREE.AxesHelper(2);
+// scene.add(axesHelper);
+
+// Object
+// const geometry = new THREE.BoxBufferGeometry(1, 1, 1, 2, 2, 2);
+
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const geometry = new THREE.BufferGeometry();
+
+// const positions = [];
+
+// for (let i = 0; i < 50; i++) {
+// Define the vertex positions as an array of numbers
+//   for (let j = 0; j < 3; j++) {
+//     const vertex = [
+//       (Math.random() - 0.5) * 4,
+//       (Math.random() - 0.5) * 4,
+//       (Math.random() - 0.5) * 4,
+//     ];
+//     positions.push(...vertex);
+//   }
+// }
+
+const geometry = new THREE.BufferGeometry();
+
+const positionsArray = new Float32Array(3 * 3);
+
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 3; j++) {
+    const vertex = [
+      (Math.random() - 0.5) * 4,
+      (Math.random() - 0.5) * 4,
+      (Math.random() - 0.5) * 4,
+    ];
+    positionsArray[i] = vertex;
+  }
+}
+
+const positionAttributes = new THREE.BufferAttribute(positionsArray, 3);
+geometry.setAttribute("position", positionAttributes);
+
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 3;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
